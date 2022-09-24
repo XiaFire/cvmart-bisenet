01ea405b55e2350c921be6754f2cf8b428e1aa39
XiaFire <86812325+XiaFire@users.noreply.github.com>
2022-09-24 23:49:59 +0800

    Update start_train.sh

--- start_train.sh
+++ start_train.sh
@@ -6,7 +6,7 @@ python preprocess.py
 
 echo "training..."
 cd /project/train/src_repo/BiSeNet
-python -m torch.distributed.launch --nproc_per_node=1 tools/train_amp.py --finetune-from ./model_final_v2_city.pth --config ./configs/cvmart.py # or bisenetv1 --config /project/train/src_repo/BiSeNet/configs/cvmart.py
+python -m torch.distributed.launch --nproc_per_node=1 tools/train_amp.py --config ./configs/cvmart.py 
 
 echo "converting"
 python tools/export_onnx.py --config /project/train/src_repo/BiSeNet/configs/cvmart.py --weight-path /project/train/models/model_final.pth --outpath /project/train/models/model.onnx
